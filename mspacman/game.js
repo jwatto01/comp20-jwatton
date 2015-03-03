b31efb847ee799e7c294b5d86232f3a74fd04fa5
jwatto01 <jacob.watton@tufts.edu>
2015-03-03 18:55:49 -0500

    Finished game.js

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,10 @@
-// Replace this with your code...
+function init() {
+     canvas = document.getElementById('game_canvas');
+     ctx = canvas.getContext('2d');
+     image = new Image();
+     image.src = 'pacman10-hp-sprite.png';
+     image.onload = function() {
+          ctx.drawImage(image, 322, 0, 464, 135, 0, 0, 464, 135);
+          ctx.drawImage(image, 100, 0, 20, 20, 2, 2, 20, 20);
+     }
+}
