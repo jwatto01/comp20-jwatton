f0f9ade3386cf85e1f3728873fff001283421992
jwatto01 <jacob.watton@tufts.edu>
2015-03-10 12:31:02 -0400

    Add lab 6 to new branch

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+function parse() {
+     r = new XMLHttpRequest();
+     r.open ("GET", "data.json", true);
+     r.onreadystatechange = function() {
+          if (r.readyState == 4) {
+               messagesDiv = document.getElementById("messages");
+               converted = JSON.parse(r.responseText);
+               for (i = 0; i < converted.length; i++) {
+                    messagesDiv.innerHTML += "<p>" + converted[i]['content'] + " " + converted[i]['username'] + "</p>";
+               }
+          }
+      }
+      r.send();
+}
+
+
